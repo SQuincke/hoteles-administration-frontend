f7c137a81f127f6f68d733bbb3911253c7f8f1cd
Patricio Carrau <pcarrau@scanntech.com>
2022-06-19 01:31:33 -0300

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetchHotels = async () => {
     try {
-        const hotels = await (await fetch("http://localhost:8080/api/hotel")).json();
+        const hotels = await (await fetch("http://localhost:8080/api/hotel/")).json();
         const hotelList = document.querySelector('[data-hotel-list]');
         document.querySelector('[data-list-header]').classList.remove('not-display');
         hotelList.innerHTML = "";
@@ -10,10 +10,10 @@ const fetchHotels = async () => {
     }
 };
 
-const appendHotelData = ({name, address, roomQty}) => {
+const appendHotelData = ({hotelId, hotelName, address, roomQty}) => {
     const clone = document.querySelector("[data-hotel-template]").content.cloneNode(true);
     const item = clone.querySelector('[data-hotel-item]');
-    item.querySelector('[data-hotel-name]').innerText = name;
+    item.querySelector('[data-hotel-name]').innerText = hotelName;
     item.querySelector('[data-hotel-address]').innerText = address;
     item.querySelector('[data-hotel-room-qty]').innerText = roomQty;
     return clone;
